5bda5f79fc556b1ce076f2974b2a127d0de162a1
george827 <kinyanjuigeorge827@gmail.com>
2023-01-12 11:37:50 +0300

    add events to handle logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,84 +1,98 @@
 import React from 'react';
+import calculate from './logic/calculate';
 
 import '../App.css';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
+  clickEvent = (e) => {
+    const result = calculate(this.state, e.target.textContent);
+    this.setState((prevState) => ({
+      ...prevState,
+      ...result,
+    }));
+  };
+
   render() {
+    const { total, next } = this.state;
     return (
       <div className="calc-container">
         <div className="calc-input-field">
-          <input className="inputs" type="text" value="0" readOnly />
+          <input className="inputs" type="text" value={next || total || 0} readOnly />
         </div>
         <ul className="rows">
           <li className="row">
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               AC
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               +/-
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               %
             </button>
-            <button className="calc_btn orange_btn" type="button">
-              &divide;
+            <button className="calc_btn orange_btn" type="button" onClick={this.clickEvent}>
+              &#247;
             </button>
           </li>
           <li className="row">
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               7
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               8
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               9
             </button>
-            <button className="calc_btn orange_btn" type="button">
-              &times;
+            <button className="calc_btn orange_btn" type="button" onClick={this.clickEvent}>
+              x
             </button>
           </li>
           <li className="row">
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               4
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               5
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               6
             </button>
-            <button className="calc_btn orange_btn" type="button">
-              &minus;
+            <button className="calc_btn orange_btn" type="button" onClick={this.clickEvent}>
+              -
             </button>
           </li>
           <li className="row">
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               1
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               2
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               3
             </button>
-            <button className="calc_btn orange_btn" type="button">
+            <button className="calc_btn orange_btn" type="button" onClick={this.clickEvent}>
               +
             </button>
           </li>
           <li className="row">
-            <button className="calc_btn btn-zero" type="button">
+            <button className="calc_btn btn-zero" onClick={this.clickEvent} type="button">
               0
             </button>
-            <button className="calc_btn" type="button">
+            <button className="calc_btn" onClick={this.clickEvent} type="button">
               .
             </button>
-            <button className="calc_btn orange_btn" type="button">
+            <button className="calc_btn orange_btn" onClick={this.clickEvent} type="button">
               =
             </button>
           </li>
